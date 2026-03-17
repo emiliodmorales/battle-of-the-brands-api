0be8188fd43abcaffe4ae6d765d27c876f7568ad
Pedro Ortiz <pedro383ortiz@yahoo.com>
2026-03-17 11:23:27 -0700

    ability and team queries initial pass done

---
+++ db/queries/abilities.js
@@ -0,0 +1,15 @@
+import db from "#db/client";
+
+export async function getAbilities() {
+  const sql = "SELECT * FROM abilities";
+  const { rows: abilities } = await db.query(sql);
+  return abilities;
+}
+
+export async function getAbilities(id) {
+  const sql = "SELECT * FROM abilities WHERE id= $1";
+  const {
+    rows: [abilities],
+  } = await db.query(sql, [id]);
+  return abilities;
+}
