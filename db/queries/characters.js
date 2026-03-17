9496eb6b1c523a6ea92134a4cf655d8aa674d4ab
Froggles <elmsterm@gmail.com>
2026-03-17 14:03:25 -0400

    add get character queries

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -31,3 +31,23 @@ export async function createCharacter({
   ]);
   return character;
 }
+
+export async function getAllCharacters() {
+  const sql = `
+    SELECT * FROM "characters"
+  `;
+  const { rows: characters } = await db.query(sql);
+  return characters;
+}
+
+export async function getCharacterById(id) {
+  const sql = `
+    SELECT * FROM "characters"
+    WHERE id=$1
+  `;
+  const {
+    rows: [character],
+  } = await db.query(sql, [id]);
+  return character;
+}
+
