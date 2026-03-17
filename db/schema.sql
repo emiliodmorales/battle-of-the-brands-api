bb7522c671c6a03be1634852a79c4bb2835a786d
Froggles <elmsterm@gmail.com>
2026-03-17 13:29:05 -0400

    fix schema errors

--- db/schema.sql
+++ db/schema.sql
@@ -31,6 +31,14 @@ CREATE TABLE
   );
 
 CREATE TABLE
+  "abilities" (
+    "id" SMALLINT PRIMARY KEY NOT NULL,
+    "cost" INTEGER NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL
+  );
+
+CREATE TABLE
   "characters" (
     "id" SERIAL PRIMARY KEY NOT NULL,
     "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
@@ -40,50 +48,43 @@ CREATE TABLE
     "hp" INTEGER NOT NULL,
     "attack" INTEGER NOT NULL,
     "defense" INTEGER NOT NULL,
-    "ability_id" INTEGER NOT NULL REFERENCES "abilities" ("id") ON DELETE SET NULL
-  );
-
-CREATE TABLE
-  "abilities" (
-    "id" SMALLINT PRIMARY KEY NOT NULL,
-    "cost" INTEGER NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(255) NOT NULL
+    "ability_id" INTEGER REFERENCES "abilities" ("id") ON DELETE SET NULL
   );
 
 CREATE TABLE
   "battles" (
     "id" SERIAL PRIMARY KEY NOT NULL,
-    "challenger" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE SET NULL,
-    "defender" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE SET NULL,
-    "winner" INTEGER NULL REFERENCES "teams" ("id") ON DELETE SET NULL
+    "challenger" INTEGER REFERENCES "teams" ("id") ON DELETE SET NULL,
+    "defender" INTEGER REFERENCES "teams" ("id") ON DELETE SET NULL,
+    "winner" INTEGER REFERENCES "teams" ("id") ON DELETE SET NULL
   );
 
 CREATE TABLE
   "followers" (
     "follower" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
     "following" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
-    UNIQUE ("follower", "following")
+    PRIMARY KEY ("follower", "following")
   );
 
 CREATE TABLE
   "favorite_characters" (
     "character_id" INTEGER NOT NULL REFERENCES "characters" ("id") ON DELETE CASCADE,
     "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
-    UNIQUE ("user_id", "character_id")
+    PRIMARY KEY ("user_id", "character_id")
   );
 
 CREATE TABLE
   "favorite_teams" (
     "team_id" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE CASCADE,
     "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
-    UNIQUE ("user_id", "team_id")
+    PRIMARY KEY ("user_id", "team_id")
   );
 
 CREATE TABLE
   "teams_characters" (
     "team_id" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE CASCADE,
-    "character_id" INTEGER NOT NULL REFERENCES "characters" ("id") ON DELETE SET NULL,
+    "character_id" INTEGER REFERENCES "characters" ("id") ON DELETE SET NULL,
     "position" SMALLINT NOT NULL,
-    UNIQUE ("team_id", "character_id")
+    PRIMARY KEY ("team_id", "character_id"),
+    UNIQUE ("team_id", "position"),
   );
