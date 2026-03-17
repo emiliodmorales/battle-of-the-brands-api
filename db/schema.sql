3cfcc70c9fd029e6026ee92014dbf08d8e7c2335
Froggles <elmsterm@gmail.com>
2026-03-17 13:13:56 -0400

    create db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,89 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS "users";
 
-CREATE TABLE users (
-  id serial PRIMARY KEY,
-  username text NOT NULL UNIQUE,
-  password text NOT NULL
-);
+DROP TABLE IF EXISTS "characters";
+
+DROP TABLE IF EXISTS "abilities";
+
+DROP TABLE IF EXISTS "battles";
+
+DROP TABLE IF EXISTS "teams";
+
+DROP TABLE IF EXISTS "teams_characters";
+
+DROP TABLE IF EXISTS "followers";
+
+DROP TABLE IF EXISTS "favorite_characters";
+
+DROP TABLE IF EXISTS "favorite_teams";
+
+CREATE TABLE
+  "users" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "username" VARCHAR(255) UNIQUE NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+  );
+
+CREATE TABLE
+  "teams" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    "name" VARCHAR(255) NOT NULL
+  );
+
+CREATE TABLE
+  "characters" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    "name" VARCHAR(255) UNIQUE NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "image" VARCHAR(255) NOT NULL,
+    "hp" INTEGER NOT NULL,
+    "attack" INTEGER NOT NULL,
+    "defense" INTEGER NOT NULL,
+    "ability_id" INTEGER NOT NULL REFERENCES "abilities" ("id") ON DELETE SET NULL
+  );
+
+CREATE TABLE
+  "abilities" (
+    "id" SMALLINT PRIMARY KEY NOT NULL,
+    "cost" INTEGER NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL
+  );
+
+CREATE TABLE
+  "battles" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "challenger" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE SET NULL,
+    "defender" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE SET NULL,
+    "winner" INTEGER NULL REFERENCES "teams" ("id") ON DELETE SET NULL
+  );
+
+CREATE TABLE
+  "followers" (
+    "follower" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    "following" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    UNIQUE ("follower", "following")
+  );
+
+CREATE TABLE
+  "favorite_characters" (
+    "character_id" INTEGER NOT NULL REFERENCES "characters" ("id") ON DELETE CASCADE,
+    "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    UNIQUE ("user_id", "character_id")
+  );
+
+CREATE TABLE
+  "favorite_teams" (
+    "team_id" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE CASCADE,
+    "user_id" INTEGER NOT NULL REFERENCES "users" ("id") ON DELETE CASCADE,
+    UNIQUE ("user_id", "team_id")
+  );
+
+CREATE TABLE
+  "teams_characters" (
+    "team_id" INTEGER NOT NULL REFERENCES "teams" ("id") ON DELETE CASCADE,
+    "character_id" INTEGER NOT NULL REFERENCES "characters" ("id") ON DELETE SET NULL,
+    "position" SMALLINT NOT NULL,
+    UNIQUE ("team_id", "character_id")
+  );
