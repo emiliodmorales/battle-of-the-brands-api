78b1d0519ca6d00a298dfffdf768147a224ecf8d
Froggles <elmsterm@gmail.com>
2026-03-18 13:32:21 -0400

    add create ability query

--- db/queries/abilities.js
+++ db/queries/abilities.js
@@ -1,5 +1,19 @@
 import db from "#db/client";
 
+export async function createAbility({ cost, name, description }) {
+  const sql = `
+    INSERT INTO abilities
+      (cost, name, description)
+    VALUES
+      ($1, $2, $3)
+    RETURNING *
+  `;
+  const {
+    rows: [ability],
+  } = await db.query(sql, [cost, name, description]);
+  return ability;
+}
+
 export async function getAbilities() {
   const sql = "SELECT * FROM abilities";
   const { rows: abilities } = await db.query(sql);
