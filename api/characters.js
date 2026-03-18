107252b4fad295b0c49a697c48870ad90413af89
mwan0906 <mwan0906@gmail.com>
2026-03-18 11:10:51 -0400

    Characters routes

--- api/characters.js
+++ api/characters.js
@@ -5,25 +5,47 @@ export default router;
 import requireBody from "#middleware/requireBody";
 import requireUser from "#middleware/requireUser";
 
+import {
+  createCharacter,
+  getAllCharacters,
+  getCharacterById,
+  updateCharacterById,
+  deleteCharacterById,
+} from "#db/queries/characters";
+
 router.get("/", async (req, res) => {
-  // TODO - Get characters
-  res.send();
+  const chars = await getAllCharacters();
+  res.send(chars);
 });
 
 router.post(
   "/",
   requireUser,
-  requireBody(["name", "description", "image"]),
+  requireBody([
+    "name",
+    "description",
+    "image",
+    "hp",
+    "attack",
+    "defense",
+    "abilityId",
+  ]),
   async (req, res) => {
-    // TODO - Make new character
-    // To clarify, are we still doing randomized HP/ATK/DEF, not user-input?
-    // Because if we're randomizing, we should probably do so in the maker function
+    const char = await createCharacter({
+      name,
+      description,
+      image,
+      hp,
+      attack,
+      defense,
+      abilityId,
+      user_id: req.user.id,
+    });
     res.status(201).send();
   },
 );
 
 router.param("id", async (req, res, next, id) => {
-  // TODO - Get character by ID
   const char = await getCharacterById(id);
   if (!char) return res.status(404).send("Character not found.");
   req.character = char;
@@ -35,12 +57,42 @@ router.get("/:id", async (req, res) => {
 });
 
 // In these two, remember to check if req.user id matches the character's creator's id
-
-router.put("/:id", requireUser, async (req, res) => {
-  // TODO - Edit character
-  res.send();
+router.use(function (req, res, next) {
+  if (req.user?.id !== req.character.user_id)
+    return res.status(401).send("Unauthorized");
+  next();
 });
 
+router.put(
+  "/:id",
+  requireBody([
+    "name",
+    "description",
+    "image",
+    "hp",
+    "attack",
+    "defense",
+    "abilityId",
+  ]),
+  async (req, res) => {
+    const { id, user_id } = req.character;
+    const char = await updateCharacterById({
+      id,
+      user_id,
+      name,
+      description,
+      image,
+      hp,
+      attack,
+      defense,
+      abilityId,
+    });
+    res.status(204).send(char);
+  },
+);
+
 router.delete("/:id", requireUser, async (req, res) => {
-  // TODO - Delete character
+  await deleteCharacterById(req.character.id).then(() =>
+    res.status(204).send(),
+  );
 });
