42c8955595c4f8d74c3e37f12eaf6a9f42ecb1fc
Froggles <elmsterm@gmail.com>
2026-03-19 10:05:18 -0400

    link routers to app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ export default app;
 
 import usersRouter from "#api/users";
 import charRouter from "#api/characters";
+import teamRouter from "#api/teams";
+import abilityRouter from "#api/abilities";
 import getUserFromToken from "#middleware/getUserFromToken";
 import handlePostgresErrors from "#middleware/handlePostgresErrors";
 import cors from "cors";
@@ -22,6 +24,8 @@ app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.use("/users", usersRouter);
 app.use("/characters", charRouter);
+app.use("/teams", teamRouter);
+app.use("/ability", abilityRouter);
 
 app.use(handlePostgresErrors);
 app.use((err, req, res, next) => {
