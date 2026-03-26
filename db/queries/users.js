7fc5a9c515ee0fec5a064f5519a36ef61f550998
Froggles <elmsterm@gmail.com>
2026-03-26 11:46:56 -0400

    update query docs

--- db/queries/users.js
+++ db/queries/users.js
@@ -2,46 +2,42 @@ import db from "#db/client";
 import bcrypt from "bcrypt";
 
 /**
- * Represents a battle
- * @typedef {object} BattleInfo
- * @property {number} challenger - The id of the challenging team
- * @property {number} defender - The id of the defending team
- * @property {number} winner - The id of the winning team
- */
-/**
- * @typedef {object} BattleHistory
- * @property {number} total_battles - The total number of battles participated in
- * @property {number} wins - How many battles its team has won
- * @property {BattleInfo[]} battle_history - An array of battles participated in
+ * @typedef {object} UserInfo
+ * @property {string} username
+ * @property {string} password
  */
 
 /**
- * Create a new user
- * @param {string} username - The desired username
- * @param {string} password - An unencrypted password
- * @param {string} image - A url for a pfp
- * @returns the new user
+ * @typedef {object} ID
+ * @property {number} id
  */
-export async function createUser(username, password, image) {
+
+/**
+ * @typedef {UserInfo & ID} User
+ */
+
+/**
+ * @param {UserInfo}
+ * @returns {Promise<User>} the new user
+ */
+export async function createUser(username, password) {
   const sql = `
   INSERT INTO users
-    (username, password, image)
+    (username, password)
   VALUES
-    ($1, $2, $3)
+    ($1, $2)
   RETURNING *
   `;
   const hashedPassword = await bcrypt.hash(password, 10);
   const {
     rows: [user],
-  } = await db.query(sql, [username, hashedPassword, image]);
+  } = await db.query(sql, [username, hashedPassword]);
   return user;
 }
 
 /**
- * Get a user by their credentials
- * @param {string} username - the user's name
- * @param {string} password - the user's unencrypted password
- * @returns the user with the given credentials
+ * @param {UserInfo}
+ * @returns {Promise<User>} the user with the given credentials
  */
 export async function getUserByUsernameAndPassword(username, password) {
   const sql = `
@@ -61,9 +57,8 @@ export async function getUserByUsernameAndPassword(username, password) {
 }
 
 /**
- * Get a user by their id
  * @param {number} id - the user's id
- * @returns the user with the given id
+ * @returns {Promise<User>} the user with the given id
  */
 export async function getUserById(id) {
   const sql = `
@@ -78,7 +73,7 @@ export async function getUserById(id) {
 }
 
 /**
- * @returns an array containing all users
+ * @returns {Promise<User[]>} an array containing all users
  */
 export async function getUsers() {
   const sql = `
@@ -90,37 +85,7 @@ export async function getUsers() {
 }
 
 /**
- * Get the battle history of a user by their id
- * @param {number} id - The id of the user
- * @returns {BattleHistory} the user's battle history
- */
-export async function getUserHistory(id) {
-  const sql = `
-    SELECT
-      json_agg(json_build_object(
-        'challenger', (SELECT teams FROM teams WHERE teams.id=battles.challenger),
-        'defender', (SELECT teams FROM teams WHERE teams.id=battles.defender),
-        'winner', (SELECT teams FROM teams WHERE teams.id=battles.winner)
-      )) AS battle_history,
-      count(battles) AS total_battles,
-      (
-        SELECT count(battles)
-        FROM battles
-        JOIN "teams" ON teams.user_id=$1
-        WHERE winner=teams.id
-      ) AS wins
-    FROM "battles"
-    JOIN "teams" ON teams.user_id=$1
-    WHERE challenger=teams.id OR defender=teams.id
-  `;
-  const {
-    rows: [history],
-  } = await db.query(sql, [id]);
-  return history;
-}
-
-/**
- * @returns everyone who FOLLOWS the user
+ * @returns {Promise<User[]>} everyone who FOLLOWS the user
  */
 export async function getUserFollowers(id) {
   const sql = `
@@ -135,7 +100,7 @@ export async function getUserFollowers(id) {
 }
 
 /**
- * @returns everybody who the USER follows
+ * @returns {Promise<User[]>} everybody who the USER follows
  */
 export async function getUserFollowing(id) {
   const sql = `
@@ -149,31 +114,44 @@ export async function getUserFollowing(id) {
   return users;
 }
 
-export async function getUserIsFollowing(followerId, followingId) {
+/**
+ * @param {number} followerId user id of the follower
+ * @param {number} userId user id being followed
+ * @returns {Promise<boolen>} whether the follower is following the user
+ */
+export async function getUserIsFollowing(followerId, userId) {
   const sql = `
     SELECT FROM followers
     WHERE follower = $1 AND following = $2
   `;
   const {
     rows: [user],
-  } = await db.query(sql, [followerId, followingId]);
+  } = await db.query(sql, [followerId, userId]);
   return user !== undefined;
 }
 
-export async function addFollower(followerId, followingId) {
+/**
+ * @param {number} followerId user id of the follower
+ * @param {number} userId user id being followed
+ */
+export async function addFollower(followerId, userId) {
   const sql = `
     INSERT INTO followers
       (follower, following)
     VALUES
       ($1, $2)
   `;
-  await db.query(sql, [followerId, followingId]);
+  await db.query(sql, [followerId, userId]);
 }
 
-export async function removeFollower(followerId, followingId) {
+/**
+ * @param {number} followerId user id of the follower
+ * @param {number} userId user id being followed
+ */
+export async function removeFollower(followerId, userId) {
   const sql = `
     DELETE FROM followers
     WHERE follower = $1 AND following = $2
   `;
-  await db.query(sql, [followerId, followingId]);
+  await db.query(sql, [followerId, userId]);
 }
